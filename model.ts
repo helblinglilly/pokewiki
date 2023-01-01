9f583828e95e49f032e59851a633abe26209eadf
Joel Helbling <helblingjoel@gmail.com>
2023-01-01 15:39:05 +0000

    Add plain console log

--- model.ts
+++ model.ts
@@ -85,6 +85,7 @@ class Model {
 			);
 		}
 
+		console.log(`Retrieved ${pokemon.length} amount Pokemon`);
 		log.debug(`Retrieved ${pokemon.length} amount Pokemon`);
 
 		if (searchTerm === undefined || pokemon.length === 0) return pokemon;

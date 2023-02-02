5529b96f15b6684cdc0f8c6ec34f83224c6ccd8e
Joel Helbling <helblingjoel@gmail.com>
2023-02-02 18:24:55 +0000

    Tidy up cookies

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,31 +1,3 @@
-const searchHistoryLimit = 5;
-
-document.addEventListener("DOMContentLoaded", event => {});
-
-const addHistory = item => {
-	const history = getCookie("history");
-
-	if (history === undefined) {
-		setCookie("history", item);
-		return;
-	}
-
-	const items = history.split(",");
-	if (items.length > searchHistoryLimit) {
-		items.pop();
-	}
-	items.push(item);
-
-	setCookie("history", items);
-};
-
-const getHistory = () => {
-	const history = getCookie("history");
-	const items = [];
-
-	return items;
-};
-
 const getCookie = name => {
 	const cookies = document.cookie.split(";");
 	for (let i = 0; i < cookies.length; i++) {
@@ -39,5 +11,5 @@ const setCookie = (name, value) => {
 	const expires = new Date();
 	expires.setDate(expires.getDate() + 90);
 
-	document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/;`;
+	document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; SameSite=Lax;`;
 };

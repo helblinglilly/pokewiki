ed85bc6fd78705ea6358faab474247954b284bcf
Joel Helbling <helblingjoel@gmail.com>
2023-02-02 19:50:44 +0000

    Update cookie from reference

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,15 +1,40 @@
+/*
+	Below functions are taken from
+	https://javascript.info/cookie
+*/
 const getCookie = name => {
-	const cookies = document.cookie.split(";");
-	for (let i = 0; i < cookies.length; i++) {
-		if (cookies[i].split("=")[0].trim() === name) {
-			return cookies[i].split("=")[1];
-		}
-	}
+	let matches = document.cookie.match(
+		new RegExp(
+			"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)"
+		)
+	);
+	return matches ? decodeURIComponent(matches[1]) : undefined;
 };
 
-const setCookie = (name, value) => {
+const setCookie = (name, value, options = {}) => {
 	const expires = new Date();
 	expires.setDate(expires.getDate() + 90);
 
-	document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; SameSite=Lax;`;
+	options = {
+		path: "/",
+		expires: expires,
+		SameSite: "Lax",
+		...options,
+	};
+
+	if (options.expires instanceof Date) {
+		options.expires = options.expires.toUTCString();
+	}
+
+	let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+	for (let optionKey in options) {
+		updatedCookie += "; " + optionKey;
+		let optionValue = options[optionKey];
+		if (optionValue !== true) {
+			updatedCookie += "=" + optionValue;
+		}
+	}
+
+	document.cookie = updatedCookie;
 };
